c64375e828761daaf8804631eafcdaaf71e147b3
Chandrashekhar Chanagonda <S912704@emirates.com>
2019-12-27 12:31:56 +0400

    eslint setup, multibrowser config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,42 +1,10 @@
-const chromedriver = require('chromedriver');
-const allure = require('nightwatch-allure-adapter');
-
-module.exports = {
-    'src_folders': ['./tests/specs/'],
-    'reporter': allure [{
-        'outputDir': './allure-results/',
-        'disableWebdriverStepsReporting': false,
-        'disableWebdriverScreenshotsReporting': false,}
-    ],
-    'output_folder': './allure-results',
-    'page_objects_path': './tests/support/pages',
-    'globals_path': './tests/support/helpers/allGlobals.js',
-    'webdriver': {
-        'start_process': true,
-        'port': 9515,
-        'server_path': chromedriver.path,
-        'cli_args': [
-            '--verbose'
-        ]
-    },
-    'test_settings': {
-        'default': {
-            'launch_url': 'https://www.binance.com/',
-            'desiredCapabilities': {
-                'browserName': 'chrome',
-                'javascriptEnabled': true,
-                'acceptSslCerts': true,
-                'chromeOptions': {
-                    'args': ['--no-sandbox', '--start-maximized']
-                }
-                ,
-            }
-        },
-        'screenshots': {
-            'enabled': true,
-            'on_failure': true,
-            'on_error': true,
-            'path': './screenshots'
-        }
+module.exports = (function (settings) {
+    if (process.env.SELENIUM_HOST) {
+        settings.selenium.host = process.env.SELENIUM_HOST;
     }
-};
+    if (process.env.SELENIUM_PORT) {
+        settings.selenium.port = process.env.SELENIUM_PORT;
+    }
+
+    return settings;
+})(require('./nightwatch.json'));
